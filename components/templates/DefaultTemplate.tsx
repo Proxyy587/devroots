716663ea90b5cb5225855744fcc5ebd677518a6d
Proxyy587 <abhijitbhattacharjee333@gmail.com>
2024-06-27 22:37:59 +0530

    template #1 ready

--- components/templates/DefaultTemplate.tsx
+++ components/templates/DefaultTemplate.tsx
@@ -1,5 +1,6 @@
-import { Badges } from "@/constants";
+import { Badges, Socials } from "@/constants";
 import Image from "next/image";
+import Link from "next/link";
 
 const DefaultTemplate = () => {
   return (
@@ -54,6 +55,54 @@ const DefaultTemplate = () => {
                       </div>
                     ))}
                   </div>
+                </div>
+              </div>
+              <div className="mt-1 rounded pt-2">
+                <div>
+                  <div className="flex flex-row flex-wrap items-center justify-center">
+                    {Socials.map((social, index) => (
+                      <Link
+                        key={index}
+                        rel="noopener noreferrer nofollow"
+                        target="_blank"
+                        aria-label={social.url.split(".")[1]}
+                        href={social.url}
+                        className="flex cursor-pointer items-center justify-center rounded-full border-white border-opacity-10 bg-opacity-10 p-1.5 transition-all"
+                      >
+                        {social.icon}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="max-w-s mt-1 w-full space-y-2">
+                <div className="bg-animate group mb-1 w-full cursor-pointer rounded bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-900 text-sm text-zinc-400 shadow transition-all">
+                  <Link
+                    href="https://abhijee.com"
+                    className="w-full transition-all sm:hover:opacity-70"
+                  >
+                    <span className="relative mt-2 flex w-full flex-row items-center justify-between rounded bg-accent text-sm leading-tight">
+                      <div className="flex items-center justify-center">
+                        <div className="flex h-16 w-16 items-center justify-center rounded bg-secondary bg-opacity-50">
+                          <span> 🌊 </span>
+                        </div>
+                        <div className="mx-4 flex flex-col">
+                          <span
+                            title="My Portfolio"
+                            className="text-ellipsis text-[16px] text-zinc-400"
+                          >
+                            My Portfolio
+                          </span>
+                          <span
+                            title="https://abhijee.com"
+                            className="xs:w-[220px] mt-1 w-[175px] truncate text-ellipsis whitespace-nowrap text-[14px] text-zinc-700 sm:w-[275px] md:w-[340px] lg:w-[400px]"
+                          >
+                            abhijee.com
+                          </span>
+                        </div>
+                      </div>
+                    </span>
+                  </Link>
                 </div>
               </div>
             </div>
